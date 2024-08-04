e5b2059e326e9a64f0caa251d64fff153eb52221
Dumindi Lochana <131225776+Dumindi18@users.noreply.github.com>
2024-08-05 00:40:22 +0530

    Add files via upload

---
+++ Q3.scala
@@ -0,0 +1,17 @@
+object PrimeFilter {
+  def isPrime(number: Int): Boolean = {
+    if (number <= 1) false
+    else if (number == 2) true
+    else !(2 until number).exists(i => number % i == 0)
+  }
+
+  def filterPrime(numbers: List[Int]): List[Int] = {
+    numbers.filter(number => isPrime(number))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val output = filterPrime(input)
+    println(output)
+  }
+}
